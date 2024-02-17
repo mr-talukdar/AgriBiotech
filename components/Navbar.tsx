230d0ca10564d846d9c4bad448f859a00ad926b2
Rahul Talukdar <42152385+mr-talukdar@users.noreply.github.com>
2024-02-17 15:59:50 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@ import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Button from "./button";
 
 const Navbar = () => {
   return (
